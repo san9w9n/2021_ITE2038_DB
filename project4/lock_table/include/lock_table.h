16589653d8d9e70c5c3f6ff4b5b66f0764a90b43
2018008877 <2018008877@hanyang.ac.kr>
2021-11-08 12:15:30 +0900

    project4 2021-11-08

--- project4/lock_table/include/lock_table.h
+++ project4/lock_table/include/lock_table.h
@@ -5,9 +5,10 @@
 
 typedef struct lock_t lock_t;
 typedef struct link_t link_t;
+typedef struct entry_t entry_t;
 
 int init_lock_table();
-lock_t *lock_acquire(int table_id, int64_t key);
+lock_t *lock_acquire(int64_t table_id, int64_t key);
 int lock_release(lock_t* lock_obj);
 
 #endif /* __LOCK_TABLE_H__ */
--- project4/lock_table/src/lock_table.cc
+++ project4/lock_table/src/lock_table.cc
@@ -14,20 +14,15 @@ struct pair_hash {
   	}
 };
 
-struct sent_point_t {
-	int table_id;
-	int64_t key;
-};
-
 struct lock_t {
 	lock_t* prev;
 	lock_t* next;
-	sent_point_t sent_point;
+	entry_t* sent_point;
 	pthread_cond_t cond;
 };
 
 struct entry_t {
-	int table_id;
+	int64_t table_id;
 	int64_t key;
 	lock_t* tail;
 	lock_t* head;
@@ -38,7 +33,7 @@ struct link_t {
 	link_t* next;
 };
 
-typedef std::unordered_map<std::pair<int, int64_t>, link_t* , pair_hash> lock_table_t;
+typedef std::unordered_map<std::pair<int64_t, int64_t>, link_t* , pair_hash> lock_table_t;
 lock_table_t lock_table;
 
 int init_lock_table() {
@@ -46,22 +41,20 @@ int init_lock_table() {
   	return 0;
 }
 
-lock_t* lock_acquire(int table_id, int64_t key) {
+lock_t* lock_acquire(int64_t table_id, int64_t key) {
 	lock_t* 				lock;
 	lock_t* 				tail;
 	entry_t* 				entry;
 	link_t* 				tmp;
 	link_t*					link;
 	bool 					flag;
-	std::pair<int, int64_t> 	key_pair;
+	std::pair<int64_t, int64_t> key_pair;
 
 	pthread_mutex_lock(&mutex);
 
 	lock = (lock_t*)malloc(sizeof(lock_t));
 	lock->prev = lock->next = nullptr;
 	lock->cond = PTHREAD_COND_INITIALIZER;
-	lock->sent_point.table_id = table_id;
-	lock->sent_point.key = key;
 
 	key_pair = {table_id, key};
 	lock_table_t::iterator it = lock_table.find(key_pair);
@@ -71,6 +64,8 @@ lock_t* lock_acquire(int table_id, int64_t key) {
 		entry->table_id = table_id;
 		entry->key = key;
 		entry->tail = entry->head = lock;
+
+		lock->sent_point = entry;
 
 		lock_table[key_pair] = (link_t*)malloc(sizeof(link_t));
 		lock_table[key_pair]->cur = entry;
@@ -86,6 +81,7 @@ lock_t* lock_acquire(int table_id, int64_t key) {
 			tmp = tmp->next;
 		}
 		if(flag) {
+			lock->sent_point = tmp->cur;
 			tail = tmp->cur->tail;
 			tail->next = lock;
 			lock->prev = tail;
@@ -97,6 +93,7 @@ lock_t* lock_acquire(int table_id, int64_t key) {
 			entry->key = key;
 			entry->tail = entry->head = lock;
 
+			lock->sent_point = entry;
 			link = (link_t*)malloc(sizeof(link_t));
 			link->cur = entry;
 			tmp->next = link;
@@ -109,54 +106,55 @@ lock_t* lock_acquire(int table_id, int64_t key) {
 };
 
 int lock_release(lock_t* lock_obj) {
-	int 					table_id;
+	int64_t					table_id;
 	int64_t 				key;
 	link_t* 				prev_tmp;
 	link_t* 				tmp;
+	entry_t*				sent_point;
 	lock_t*					del;
-	std::pair<int, int64_t> 	key_pair;
+	std::pair<int64_t, int64_t> key_pair;
 
 	pthread_mutex_lock(&mutex);
 
-	table_id = lock_obj->sent_point.table_id;
-	key = lock_obj->sent_point.key;
+	sent_point = lock_obj->sent_point;
+	table_id = sent_point->table_id;
+	key = sent_point->key;
 	key_pair = {table_id, key};
 
-	prev_tmp = nullptr;
-	tmp = lock_table[key_pair];
-	if(!tmp || !tmp->cur) {
-		pthread_mutex_unlock(&mutex);
-		return 1;
-	}
-
-	while(tmp->cur->key!=key || tmp->cur->table_id!=table_id) {
-		prev_tmp = tmp;
-		tmp = tmp->next;
-		if(!tmp) {
-			pthread_mutex_unlock(&mutex);
-			return 1;
+	if(sent_point->head != sent_point->tail) {
+		if(sent_point->head!=lock_obj) {
+			perror("WRONG LOCK_OBJ!!");
+			exit(EXIT_FAILURE);
 		}
-	}
-	if(tmp->cur->head == tmp->cur->tail) {
-		free(tmp->cur->head);
+		del = sent_point->head;
+		sent_point->head = del->next;
+		sent_point->head->prev = nullptr;
+		free(del);
+
+		pthread_cond_signal(&sent_point->head->cond);
+	} else {
+		prev_tmp = nullptr;
+		tmp = lock_table[key_pair];
+		while(tmp->cur!=sent_point) {
+			prev_tmp = tmp;
+			tmp = tmp->next;
+			if(!tmp) {
+				perror("WRONG LOGIC!!");
+				exit(EXIT_FAILURE);
+			}
+		}
+		free(sent_point->head);
 		if(prev_tmp) {
 			prev_tmp->next = tmp->next;
-			free(tmp->cur);
+			free(sent_point);
 			free(tmp);
 		} else {
 			lock_table[key_pair] = tmp->next;
-			free(tmp->cur);
+			if(!tmp->next) lock_table.erase(key_pair);
+			free(sent_point);
 			free(tmp);
-			if(!lock_table[key_pair]) lock_table.erase(key_pair);
 		}
-	} else {
-		del = tmp->cur->head;
-		tmp->cur->head = tmp->cur->head->next;
-		tmp->cur->head->prev = nullptr;
-		pthread_cond_signal(&tmp->cur->head->cond);
-		free(del);
 	}
-
 	pthread_mutex_unlock(&mutex);
   	return 0;
 }
