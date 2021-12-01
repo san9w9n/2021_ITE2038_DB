4da0cb791d421c10c874720590a6a1b0b3688be7
2018008877 <2018008877@hanyang.ac.kr>
2021-12-01 20:49:51 +0900

    buffer mod

--- project5/db_project/db/src/buffer.cc
+++ project5/db_project/db/src/buffer.cc
@@ -6,19 +6,23 @@
 #pragma gcc optimization("O3");
 #pragma gcc optimization("unroll-loops");
 
-buffer_pool_t* buffer = NULL;
+frame_t* frames;
+int num_frames;
+int num_bufs;
+int firstLRU;
+int lastLRU;
 pthread_mutex_t buf_mutex;
 
 int buf_hashFunction(int64_t table_id, pagenum_t pagenum) {
-    int ret = (pagenum+table_id)<<2;
-    return ret % buffer->num_bufs;
+    int ret = std::hash<int64_t>()(table_id) ^ std::hash<int64_t>()(pagenum);
+    return ret % num_bufs;
 }
 
 int find_empty_frame(int64_t table_id, pagenum_t pagenum) {
     int i, tablesize;
-    tablesize = buffer->num_bufs;
+    tablesize = num_bufs;
     i = buf_hashFunction(table_id, pagenum);
-    while(buffer->frames[i].is_buf) {
+    while(frames[i].is_buf) {
         i = (i+1)%tablesize;
     }
     append_LRU(i);
@@ -27,11 +31,11 @@ int find_empty_frame(int64_t table_id, pagenum_t pagenum) {
 
 int hit_idx(int64_t table_id, pagenum_t pagenum) {
     int i, hashValue, tablesize;
-    tablesize = buffer->num_bufs;
+    tablesize = num_bufs;
     hashValue = buf_hashFunction(table_id, pagenum);
     i = hashValue;
     while(1) {
-        if(buffer->frames[i].is_buf && buffer->frames[i].table_id==table_id && buffer->frames[i].page_num==pagenum) {
+        if(frames[i].is_buf && frames[i].table_id==table_id && frames[i].page_num==pagenum) {
             delete_append_LRU(i);
             return i;
         }
@@ -42,53 +46,53 @@ int hit_idx(int64_t table_id, pagenum_t pagenum) {
 }
 
 int isValid(int64_t table_id) {
-    if(!buffer || !isValid_table_id(table_id)) return 0;
+    if(!frames || !isValid_table_id(table_id)) return 0;
     return 1;
 }
 
 void append_LRU(int idx) {
-    if(!buffer->num_frames) {
-        buffer->firstLRU = buffer->lastLRU = idx;
-        buffer->frames[idx].prevLRU = buffer->frames[idx].nextLRU = -1;
-        buffer->num_frames++;
+    if(!num_frames) {
+        firstLRU = lastLRU = idx;
+        frames[idx].prevLRU = frames[idx].nextLRU = -1;
+        num_frames++;
         return;
     }
-    if(buffer->lastLRU>=0) buffer->frames[buffer->lastLRU].nextLRU = idx;
-    buffer->frames[idx].prevLRU = buffer->lastLRU;
-    buffer->frames[idx].nextLRU = -1;
-    buffer->lastLRU = idx;
-    buffer->num_frames++;
+    if(lastLRU>=0) frames[lastLRU].nextLRU = idx;
+    frames[idx].prevLRU = lastLRU;
+    frames[idx].nextLRU = -1;
+    lastLRU = idx;
+    num_frames++;
 }
 
 void delete_LRU(int idx) {
-    if(buffer->firstLRU==idx) buffer->firstLRU = buffer->frames[idx].nextLRU;
-    if(buffer->lastLRU==idx) buffer->lastLRU = buffer->frames[idx].prevLRU;
-    if(buffer->frames[idx].prevLRU>=0) 
-        buffer->frames[buffer->frames[idx].prevLRU].nextLRU = buffer->frames[idx].nextLRU;
-    if(buffer->frames[idx].nextLRU>=0) 
-        buffer->frames[buffer->frames[idx].nextLRU].prevLRU = buffer->frames[idx].prevLRU;
-    buffer->frames[idx].nextLRU = buffer->frames[idx].prevLRU = -1;
-    buffer->num_frames--;
+    if(firstLRU==idx) firstLRU = frames[idx].nextLRU;
+    if(lastLRU==idx)  lastLRU = frames[idx].prevLRU;
+    if(frames[idx].prevLRU>=0) 
+        frames[frames[idx].prevLRU].nextLRU = frames[idx].nextLRU;
+    if(frames[idx].nextLRU>=0) 
+        frames[frames[idx].nextLRU].prevLRU = frames[idx].prevLRU;
+    frames[idx].nextLRU = frames[idx].prevLRU = -1;
+    num_frames--;
 }
 
 void delete_append_LRU(int idx) {
-    if(buffer->lastLRU==idx) return;
+    if(lastLRU==idx) return;
     delete_LRU(idx);
     append_LRU(idx);
 }
 
 int give_idx() {
     int ret_idx = -1;
-    int i = buffer->firstLRU;
+    int i = firstLRU;
 
     while(ret_idx<0) {
-        if(buffer->frames[i].state == UNLOCKED) {
-            if(buffer->frames[i].is_dirty)
-                file_write_page(buffer->frames[i].table_id, buffer->frames[i].page_num, buffer->frames[i].page);
-            memset(buffer->frames[i].page, 0x00, PGSIZE);
+        if(frames[i].state == UNLOCKED) {
+            if(frames[i].is_dirty)
+                file_write_page(frames[i].table_id, frames[i].page_num, frames[i].page);
+            memset(frames[i].page, 0x00, PGSIZE);
             ret_idx = i;
         }
-        i = buffer->frames[i].nextLRU;
+        i = frames[i].nextLRU;
         if(i<0) break;
     }
     if(i<0 && ret_idx<0) {
@@ -99,22 +103,21 @@ int give_idx() {
 }
 
 int init_buffer(int num_buf) {
-    if(!buffer) {
-        buffer = (buffer_pool_t*)malloc(sizeof(buffer_pool_t));
+    if(!frames) {
         buf_mutex = PTHREAD_MUTEX_INITIALIZER;
         if(num_buf<5) num_buf = 5;
-        buffer->frames = (frame_t*)malloc(sizeof(frame_t)*num_buf);
+        frames = (frame_t*)malloc(sizeof(frame_t)*num_buf);
         for(int i=0; i<num_buf; i++) {
-            buffer->frames[i].page = (page_t*)malloc(PGSIZE);
-            memset(buffer->frames[i].page, 0x00, PGSIZE);
-            buffer->frames[i].page_num = 0;
-            buffer->frames[i].nextLRU = buffer->frames[i].prevLRU = -1;
-            buffer->frames[i].table_id = buffer->frames[i].is_dirty = buffer->frames[i].is_buf = 0;
-            buffer->frames[i].page_mutex = PTHREAD_MUTEX_INITIALIZER;
+            frames[i].page = (page_t*)malloc(PGSIZE);
+            memset(frames[i].page, 0x00, PGSIZE);
+            frames[i].page_num = 0;
+            frames[i].nextLRU = frames[i].prevLRU = -1;
+            frames[i].table_id = frames[i].is_dirty = frames[i].is_buf = 0;
+            frames[i].page_mutex = PTHREAD_MUTEX_INITIALIZER;
         }
-        buffer->num_frames = 0;
-        buffer->num_bufs = num_buf;
-        buffer->firstLRU = buffer->lastLRU = -1;
+        num_frames = 0;
+        num_bufs = num_buf;
+        firstLRU = lastLRU = -1;
         return 0;
     }
     return 1;
@@ -134,85 +137,85 @@ pagenum_t buffer_alloc_page(int64_t table_id) {
     LOCK(buf_mutex);
     hit = hit_idx(table_id, 0);
     if(hit>=0) {
-        buffer->frames[hit].state = LOCKED;
-        LOCK(buffer->frames[hit].page_mutex);
+        frames[hit].state = LOCKED;
+        LOCK(frames[hit].page_mutex);
 
-        if(!buffer->frames[hit].page->nextfree_num) {
-            if(buffer->frames[hit].is_dirty) file_write_page(table_id, 0, buffer->frames[hit].page);
-            buffer->frames[hit].is_dirty = 0;
+        if(!frames[hit].page->nextfree_num) {
+            if(frames[hit].is_dirty) file_write_page(table_id, 0, frames[hit].page);
+            frames[hit].is_dirty = 0;
             new_pagenum = file_alloc_page(table_id); // file size become twice
 
-            if(buffer->num_frames < buffer->num_bufs) new_idx = find_empty_frame(table_id, new_pagenum);
+            if(num_frames < num_bufs) new_idx = find_empty_frame(table_id, new_pagenum);
             else new_idx = give_idx();
 
-            buffer->frames[new_idx].state = LOCKED;
-            LOCK(buffer->frames[new_idx].page_mutex);
+            frames[new_idx].state = LOCKED;
+            LOCK(frames[new_idx].page_mutex);
             UNLOCK(buf_mutex);
 
-            buffer->frames[new_idx].is_buf = 1;
-            buffer->frames[new_idx].is_dirty = 0;
-            buffer->frames[new_idx].table_id = table_id;
+            frames[new_idx].is_buf = 1;
+            frames[new_idx].is_dirty = 0;
+            frames[new_idx].table_id = table_id;
 
-            file_read_page(table_id, 0, buffer->frames[hit].page);
+            file_read_page(table_id, 0, frames[hit].page);
         }
         
         // subcase 2 : File has a freepage.
         else {
-            new_pagenum = buffer->frames[hit].page->nextfree_num;
-            if(buffer->num_frames < buffer->num_bufs) new_idx = find_empty_frame(table_id, new_pagenum);
+            new_pagenum = frames[hit].page->nextfree_num;
+            if(num_frames < num_bufs) new_idx = find_empty_frame(table_id, new_pagenum);
             else new_idx = give_idx();
 
-            buffer->frames[new_idx].state = LOCKED;
-            LOCK(buffer->frames[new_idx].page_mutex);
+            frames[new_idx].state = LOCKED;
+            LOCK(frames[new_idx].page_mutex);
             UNLOCK(buf_mutex);
 
-            buffer->frames[new_idx].is_buf = 1;
-            buffer->frames[new_idx].is_dirty = 0;
-            buffer->frames[new_idx].table_id = table_id;
+            frames[new_idx].is_buf = 1;
+            frames[new_idx].is_dirty = 0;
+            frames[new_idx].table_id = table_id;
 
-            file_read_page(table_id, new_pagenum, buffer->frames[new_idx].page);
-            buffer->frames[hit].page->nextfree_num = buffer->frames[new_idx].page->nextfree_num;
-            buffer->frames[hit].is_dirty = 1;
+            file_read_page(table_id, new_pagenum, frames[new_idx].page);
+            frames[hit].page->nextfree_num = frames[new_idx].page->nextfree_num;
+            frames[hit].is_dirty = 1;
         }
 
-        buffer->frames[new_idx].page_num = new_pagenum;
+        frames[new_idx].page_num = new_pagenum;
 
-        buffer->frames[hit].state = UNLOCKED;
-        UNLOCK(buffer->frames[hit].page_mutex);
+        frames[hit].state = UNLOCKED;
+        UNLOCK(frames[hit].page_mutex);
 
-        buffer->frames[new_idx].state = UNLOCKED;
-        UNLOCK(buffer->frames[new_idx].page_mutex);
+        frames[new_idx].state = UNLOCKED;
+        UNLOCK(frames[new_idx].page_mutex);
 
         return new_pagenum;
     }
 
     // case 2: header page is not in buffer.
     new_pagenum = file_alloc_page(table_id);
-    if(buffer->num_frames<buffer->num_bufs) hit = find_empty_frame(table_id, new_pagenum);
+    if(num_frames<num_bufs) hit = find_empty_frame(table_id, new_pagenum);
     else hit = give_idx();
-    buffer->frames[hit].state = LOCKED;
-    LOCK(buffer->frames[hit].page_mutex);
+    frames[hit].state = LOCKED;
+    LOCK(frames[hit].page_mutex);
 
-    buffer->frames[hit].is_dirty = 0;
-    buffer->frames[hit].is_buf = 1;
-    buffer->frames[hit].page_num = 0;
-    buffer->frames[hit].table_id = table_id;
-    file_read_page(table_id, 0, buffer->frames[hit].page);
+    frames[hit].is_dirty = 0;
+    frames[hit].is_buf = 1;
+    frames[hit].page_num = 0;
+    frames[hit].table_id = table_id;
+    file_read_page(table_id, 0, frames[hit].page);
     
-    if(buffer->num_frames < buffer->num_bufs) new_idx = find_empty_frame(table_id, new_pagenum);
+    if(num_frames < num_bufs) new_idx = find_empty_frame(table_id, new_pagenum);
     else new_idx = give_idx();
 
-    buffer->frames[new_idx].state = LOCKED;
-    LOCK(buffer->frames[new_idx].page_mutex);
+    frames[new_idx].state = LOCKED;
+    LOCK(frames[new_idx].page_mutex);
     UNLOCK(buf_mutex);
 
-    buffer->frames[new_idx].is_buf = 1;
-    buffer->frames[new_idx].is_dirty = 0;
-    buffer->frames[new_idx].table_id = table_id;
-    buffer->frames[new_idx].page_num = new_pagenum;
+    frames[new_idx].is_buf = 1;
+    frames[new_idx].is_dirty = 0;
+    frames[new_idx].table_id = table_id;
+    frames[new_idx].page_num = new_pagenum;
 
-    UNLOCK(buffer->frames[hit].page_mutex);
-    UNLOCK(buffer->frames[new_idx].page_mutex);
+    UNLOCK(frames[hit].page_mutex);
+    UNLOCK(frames[new_idx].page_mutex);
 
     return new_pagenum;    
 }
@@ -220,45 +223,45 @@ pagenum_t buffer_alloc_page(int64_t table_id) {
 void buffer_free_page(int64_t table_id, pagenum_t pagenum, int32_t idx) {
     int hit;
     LOCK(buf_mutex);
-    memset(buffer->frames[idx].page, 0x00, PGSIZE);
-    buffer->frames[idx].is_buf = buffer->frames[idx].is_dirty = 0;
-    buffer->frames[idx].table_id = 0;
-    buffer->frames[idx].page_num = 0;
+    memset(frames[idx].page, 0x00, PGSIZE);
+    frames[idx].is_buf = frames[idx].is_dirty = 0;
+    frames[idx].table_id = 0;
+    frames[idx].page_num = 0;
     delete_LRU(idx);
 
     hit = hit_idx(table_id, 0);
     // case 1: header page is in buffer.
     if(hit>=0) {
-        buffer->frames[hit].state = LOCKED;
-        LOCK(buffer->frames[hit].page_mutex);
+        frames[hit].state = LOCKED;
+        LOCK(frames[hit].page_mutex);
         UNLOCK(buf_mutex);
-        buffer->frames[idx].page->nextfree_num = buffer->frames[hit].page->nextfree_num;
-        file_write_page(table_id, pagenum, buffer->frames[idx].page);
-        buffer->frames[hit].page->nextfree_num = pagenum;
-        buffer->frames[hit].is_dirty = 1;
-        UNLOCK(buffer->frames[hit].page_mutex);
-        UNLOCK(buffer->frames[idx].page_mutex);
-        buffer->frames[idx].state = UNLOCKED;
-        buffer->frames[hit].state = UNLOCKED;
+        frames[idx].page->nextfree_num = frames[hit].page->nextfree_num;
+        file_write_page(table_id, pagenum, frames[idx].page);
+        frames[hit].page->nextfree_num = pagenum;
+        frames[hit].is_dirty = 1;
+        UNLOCK(frames[hit].page_mutex);
+        UNLOCK(frames[idx].page_mutex);
+        frames[idx].state = UNLOCKED;
+        frames[hit].state = UNLOCKED;
         return;
     }
 
     // case 2: header page is not in buffer.
-    UNLOCK(buffer->frames[idx].page_mutex);
-    buffer->frames[idx].state = UNLOCKED;
+    UNLOCK(frames[idx].page_mutex);
+    frames[idx].state = UNLOCKED;
 
     file_free_page(table_id, pagenum);
     hit = find_empty_frame(table_id, 0);
-    buffer->frames[hit].state = LOCKED;
-    LOCK(buffer->frames[hit].page_mutex);
+    frames[hit].state = LOCKED;
+    LOCK(frames[hit].page_mutex);
     UNLOCK(buf_mutex);
-    buffer->frames[hit].is_dirty = 0;
-    buffer->frames[hit].is_buf = 1;
-    buffer->frames[hit].page_num = 0;
-    buffer->frames[hit].table_id = table_id;
-    file_read_page(table_id, 0, buffer->frames[hit].page);
-    UNLOCK(buffer->frames[hit].page_mutex);
-    buffer->frames[hit].state = UNLOCKED;
+    frames[hit].is_dirty = 0;
+    frames[hit].is_buf = 1;
+    frames[hit].page_num = 0;
+    frames[hit].table_id = table_id;
+    file_read_page(table_id, 0, frames[hit].page);
+    UNLOCK(frames[hit].page_mutex);
+    frames[hit].state = UNLOCKED;
 }
 
 page_t* buffer_read_page(int64_t table_id, pagenum_t pagenum, int* idx, bool mode) {
@@ -267,57 +270,55 @@ page_t* buffer_read_page(int64_t table_id, pagenum_t pagenum, int* idx, bool mod
     LOCK(buf_mutex);
     hit = hit_idx(table_id, pagenum);
     if(hit>=0) {
-        buffer->frames[hit].state = LOCKED;
-        LOCK(buffer->frames[hit].page_mutex);
+        frames[hit].state = LOCKED;
+        LOCK(frames[hit].page_mutex);
         UNLOCK(buf_mutex);
         *idx = hit;
-        buffer->frames[hit].is_buf = 1;
-        ret = buffer->frames[hit].page;
+        frames[hit].is_buf = 1;
+        ret = frames[hit].page;
         if(mode == READ) {
-            buffer->frames[hit].state = UNLOCKED;
-            UNLOCK(buffer->frames[hit].page_mutex);
+            frames[hit].state = UNLOCKED;
+            UNLOCK(frames[hit].page_mutex);
         }
         return ret;
     }
 
-    if(buffer->num_frames<buffer->num_bufs) hit = find_empty_frame(table_id, pagenum);
+    if(num_frames<num_bufs) hit = find_empty_frame(table_id, pagenum);
     else hit = give_idx();
 
-    buffer->frames[hit].state = LOCKED;
-    LOCK(buffer->frames[hit].page_mutex);
+    frames[hit].state = LOCKED;
+    LOCK(frames[hit].page_mutex);
     UNLOCK(buf_mutex);
     *idx = hit;
-    file_read_page(table_id, pagenum, buffer->frames[hit].page);
-    buffer->frames[hit].is_buf = 1;
-    buffer->frames[hit].is_dirty = 0;
-    buffer->frames[hit].table_id = table_id;
-    buffer->frames[hit].page_num = pagenum;
-    ret = buffer->frames[hit].page;
+    file_read_page(table_id, pagenum, frames[hit].page);
+    frames[hit].is_buf = 1;
+    frames[hit].is_dirty = 0;
+    frames[hit].table_id = table_id;
+    frames[hit].page_num = pagenum;
+    ret = frames[hit].page;
     if(mode == READ) {
-        buffer->frames[hit].state = UNLOCKED;
-        UNLOCK(buffer->frames[hit].page_mutex);
+        frames[hit].state = UNLOCKED;
+        UNLOCK(frames[hit].page_mutex);
     }
     return ret;
 }
 
 void buffer_write_page(int64_t table_id, pagenum_t pagenum, int32_t idx, bool success) {
-    if(success) buffer->frames[idx].is_dirty = 1;
-    buffer->frames[idx].state = UNLOCKED;
-    UNLOCK(buffer->frames[idx].page_mutex);
+    if(success) frames[idx].is_dirty = 1;
+    frames[idx].state = UNLOCKED;
+    UNLOCK(frames[idx].page_mutex);
 }
 
 int shutdown_buffer() {
-    for(int i=0; i<buffer->num_bufs; i++) {
-        if(buffer->frames[i].is_buf && buffer->frames[i].is_dirty) {
-            file_write_page( buffer->frames[i].table_id, buffer->frames[i].page_num, buffer->frames[i].page);
+    for(int i=0; i<num_bufs; i++) {
+        if(frames[i].is_buf && frames[i].is_dirty) {
+            file_write_page( frames[i].table_id, frames[i].page_num, frames[i].page);
         }
-        free(buffer->frames[i].page);
-        buffer->frames[i].page = NULL;
+        free(frames[i].page);
+        frames[i].page = NULL;
     }
-    free(buffer->frames);
-    buffer->frames = NULL;
-    free(buffer);
-    buffer = NULL;
+    free(frames);
+    frames = NULL;
     file_close_table_files();
     return 0;
 }
